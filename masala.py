7972cb09feeedeb0a7c6a73aab55637d2508316e
liuchao <1039383677@qq.com>
2021-01-15 09:20:08 +0800

    modify style

--- masala.py
+++ masala.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, url_for,request, Response
-from flask_login import LoginManager, login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user
 
+from auth import login_manager
 from models import AuthModel
 
 app=Flask(__name__)
 app.secret_key=r'masala'
 
-login_manager=LoginManager(app)
+login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(id):
