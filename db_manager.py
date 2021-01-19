68d1c270abae19f9d67850638e3c490378dba667
liuchao <1039383677@qq.com>
2021-01-19 15:32:43 +0800

    modify register

--- db_manager.py
+++ db_manager.py
@@ -1,4 +1,7 @@
 from pony.orm import *
+from flask_login import UserMixin
+import datetime
+import copy
 
 db = Database()
 # MySQL
@@ -8,8 +11,21 @@ class Mas_User(db.Entity):
     id=PrimaryKey(int)
     name=Required(str)
     pwd=Required(str)
+    email=Required(str)
+    gender_id=Required(int)
+    birthday=Required(datetime.date)
+    age=Optional(int)
+    state_id=Required(int)
+    edubackground_id=Required(int)
+    income_id=Required(int)
+    province_id=Optional(int)
+    city_id=Optional(int)
+    height=Optional(str)
+    weight=Optional(str)
+    register_time=Required(datetime.datetime)
+    last_access=Required(datetime.datetime)
 
-db.generate_mapping(create_tables=False)
+db.generate_mapping(create_tables=True)
 
 @db_session
 def get_user_by_id(uid):
@@ -18,3 +34,7 @@ def get_user_by_id(uid):
 @db_session
 def get_user_by_md5(pwdmd5):
     return select(u for u in Mas_User if u.pwd == pwdmd5).first()
+
+@db_session
+def create_user(**reg_info):
+    pass
--- masala.py
+++ masala.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, url_for,request,redirect,flash
 from flask_login import login_required, login_user, logout_user
 from flask_wtf.csrf import CSRFProtect
 import hashlib
+from flask_socketio import SocketIO
+from datetime import date,datetime
 
 from auth import login_manager
-from models import AuthModel
 from db_manager import get_user_by_id,get_user_by_md5,Mas_User
 
 app=Flask(__name__)
@@ -15,12 +16,10 @@ csrf = CSRFProtect(app)
 
 @login_manager.user_loader
 def load_user(id):
-    u = AuthModel()
-    u.id=1
-    return u
+    return get_user_by_id(id)
 
 @app.route('/')
-#@login_required
+@login_required
 def index():
     return render_template('index.html')
 
@@ -41,7 +40,7 @@ def login():
         if u is not None:
             m_user = AuthModel()
             m_user.id=u.id
-            login_user(m_user)
+            login_user(m_user,reme)
             return redirect('/')
         flash('用户名或密码错误','login')
         return redirect('login')
@@ -57,5 +56,21 @@ def register():
     if request.method=='GET':
         return render_template('register.html')
     if request.method=='POST':
-        return None
+        #必填
+        name = request.form['name']
+        email = request.form['email']
+        pwd = request.form['pwd']
+        pwd2 = request.form['pwd2']
+        gender_id=request.form['gender_id']
+        birthday = request.form['birthday']
+        state_id = request.form['state']
+        edubackground_id = request.form['edubackground_id']
+        income_id = request.form['income_id']
+        province_id = request.form['province']
+        city_id = request.form['city']
+        height = request['height']
+        weight = request['weight']
+        register_time = datetime.now()
+        last_access = datetime.now()
+        return 'ok'
     return None
